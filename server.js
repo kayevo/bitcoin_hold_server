db6e7bca7d8c1095d1fb7f0a490947887a7ec524
johnatan_kayevo <johnatanc@ciandt.com>
2022-10-16 17:21:34 -0300

    fix: send id when verify user

--- server.js
+++ server.js
@@ -30,17 +30,20 @@ app.get("/user", (req, res) => {
     res.status(400).json(); // bad request
   } else {
     try {
-      User.find({ email: credential.email, password: credential.password }, function (err, user) {
-        if (err) {
-          res.status(500).json({ error: err });
-        } else {
-          if (user.length == 0) {
-            res.status(404).json(); // not found
+      User.find(
+        { email: credential.email, password: credential.password },
+        function (err, user) {
+          if (err) {
+            res.status(500).json({ error: err });
           } else {
-            res.status(200).json(user.id);
+            if (user.length == 0) {
+              res.status(404).json(); // not found
+            } else {
+              res.status(200).send(JSON.stringify(user[0].email));
+            }
           }
         }
-      });
+      );
     } catch (error) {
       res.status(500).json();
     }
@@ -54,7 +57,7 @@ app.get("/user/email", (req, res) => {
     res.status(400).json(); // bad request
   } else {
     try {
-      User.find({ email: _email}, function (err, user) {
+      User.find({ email: _email }, function (err, user) {
         if (err) {
           res.status(500).json({ error: err });
         } else {
