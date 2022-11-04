75e2065e5fbf74451d8232c034d3b2c7c467c2bc
johnatan_kayevo <johnatanc@ciandt.com>
2022-11-04 16:18:56 -0300

    feat: index email field

--- entity/UserEntity.js
+++ entity/UserEntity.js
@@ -1,20 +1,31 @@
-const mongoose = require('mongoose') 
-const encrypt = require('mongoose-encryption');
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  email: {
+    type: String,
+    required: true,
+    index: true, // index field
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   bitcoinPortfolio: {
     satoshiAmount: Number, // integer number in satoshis
     bitcoinAveragePrice: Number, // decimal number with 2 decimals precision
-  }
+  },
 });
 
-var encKey = process.env.ENC_KEY_32BYTE_BASE64
-var sigKey = process.env.SIG_KEY_64BYTE_BASE64
+var encKey = process.env.ENC_KEY_32BYTE_BASE64;
+var sigKey = process.env.SIG_KEY_64BYTE_BASE64;
 
-userSchema.plugin(encrypt, { encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['password']})
+userSchema.plugin(encrypt, {
+  encryptionKey: encKey,
+  signingKey: sigKey,
+  encryptedFields: ["password"],
+});
 
-const UserEntity = mongoose.model('User', userSchema)
+const UserEntity = mongoose.model("User", userSchema);
 
-module.exports = UserEntity
+module.exports = UserEntity;
