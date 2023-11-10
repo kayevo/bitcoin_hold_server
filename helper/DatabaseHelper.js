48b157034dab4c8cd131b3d9b1535bc0ce749e58
Johnatan Kayevo <johnatanc@ciandt.com>
2023-11-10 20:15:09 -0300

    refact: clean code

--- helper/DatabaseHelper.js
+++ helper/DatabaseHelper.js
@@ -1,9 +1,8 @@
 const UserEntity = require(".././entity/UserEntity");
 
 const deleteMockUsers = () => {
-  // delete users where email is less than 4
   UserEntity.deleteMany(
-    { $expr: { $lt: [{ $strLenCP: "$email" }, 4] } },
+    { $expr: { $lt: [{ $strLenCP: "$email" }, 4] } }, // delete users where email is less than 4
     (err) => {
       if (err) {
         console.error("Error deleting users:", err);
