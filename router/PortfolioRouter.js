967028ca329e537d48e9017532318e9aae343491
Kayevo <kayevo@protonmail.com>
2023-12-14 17:09:09 -0300

    fix: router method

--- router/PortfolioRouter.js
+++ router/PortfolioRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", PortfolioController.getPortfolio);
 router.post("/customize", PortfolioController.setPortfolio);
-router.post("/add", PortfolioController.addValue);
-router.post("/remove", PortfolioController.removeValue);
+router.post("/add", PortfolioController.addFunds);
+router.post("/remove", PortfolioController.removeFunds);
 
 module.exports = router;
